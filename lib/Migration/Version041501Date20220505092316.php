8100f76be1c0c7cd1571f88bb3a4d3e8c5214fbd
Manuel Biertz <kontakt@manuelbiertz.de>
2022-05-05 11:58:17 +0200

    store tokens in TEXT or CLOBs
    The current data format is translated into MySQL as varchar(255), which is not sufficient to store tokens. However, token size greatly depends on the systems, algorithms, and total information included in a token. It is thus recommended to store them in TEXT format: https://stackoverflow.com/a/5050559. Yet, this decision might decrease performance, since MySQL for instance does not keep TEXT fields ready in memory. In the future, we *might* have to reverse this decision.

---
+++ lib/Migration/Version041501Date20220505092316.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\SocialLogin\Migration;
+
+use Closure;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\DBAL\Types\Type;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version041501Date20220505092316 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if ($schema->hasTable('sociallogin_tokens')) {
+            $table = $schema->getTable('sociallogin_tokens');
+            $table->changeColumn('access_token', [
+                'Type' => Type::getType(Types::TEXT)
+            ]);
+            $table->changeColumn('refresh_token', [
+                'Type' => Type::getType(Types::TEXT)
+            ]);
+        }
+        return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+}
